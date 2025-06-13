f81344793b5cb36ac973f67858601c27d5506f7d
stella-devp <rhstella.ksea@gmail.com>
2025-06-13 06:35:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-AUTHORIZED_TOKEN = os.environ.get("API_AUTH_TOKEN", "BR Wissda2025")
+AUTHORIZED_TOKEN = os.environ.get("API_AUTH_TOKEN", "xxx")
 @app.route('/convert', methods=['POST'])
 def convert_pdf_to_docx():
     auth_header = request.headers.get('Authorization')
